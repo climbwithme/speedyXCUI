dc700af3e3f8df7ef9c3f6d72ae72cae6bdefdd6
Manjunath S C P <manjunathscp.2030@gmail.com>
2020-01-30 00:22:35 +0530

    This is the basic setup commit

---
+++ speedyXCUI/speedyXCUI/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  speedyXCUI
+//
+//  Created by Manjunath Chandrabose Pandiarajan on 30/01/20.
+//  Copyright © 2020 Manjunath Chandrabose Pandiarajan. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doThis() -> String {
+        return "This is a string"
+    }
+    
+}
---
+++ speedyXCUI/speedyXCUI/speedyXCUI.h
@@ -0,0 +1,19 @@
+//
+//  speedyXCUI.h
+//  speedyXCUI
+//
+//  Created by Manjunath Chandrabose Pandiarajan on 30/01/20.
+//  Copyright © 2020 Manjunath Chandrabose Pandiarajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for speedyXCUI.
+FOUNDATION_EXPORT double speedyXCUIVersionNumber;
+
+//! Project version string for speedyXCUI.
+FOUNDATION_EXPORT const unsigned char speedyXCUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <speedyXCUI/PublicHeader.h>
+
+
